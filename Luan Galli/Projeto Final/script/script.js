7ac96f5459569fc1cc78ce0264925f7e95836809
Luan Galli de Sousa <luan.galli@dinfor.uniceub>
2025-11-18 19:39:51 -0300

    Atualização do projeto Final

---
+++ Luan Galli/Projeto Final/script/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // ----------------------
+    // Rolagem suave para os links da navegação
+    // ----------------------
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 50,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
